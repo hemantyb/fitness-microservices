5d2e9c0a42af23225782ce92c5f4c7513cc0cbe0
Hemant Baviskar <hemantyb22@gmail.com>
2025-07-09 16:43:49 +0530

    add keycloak

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -19,26 +19,32 @@ public class UserService {
     public UserResponse register(@Valid RegisterRequest request) {
 
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new RuntimeException("Email already exists");
+            User existingUser = userRepository.findByEmail(request.getEmail());
+            return getUserResponse(existingUser);
         }
 
         User user = new User();
         user.setEmail(request.getEmail());
         user.setPassword(request.getPassword());
+        user.setKeycloakId(request.getKeycloakId());
         user.setFirstName(request.getFirstName());
         user.setLastName(request.getLastName());
 
         User savedUser = userRepository.save(user);
 
-        UserResponse userResponse = new UserResponse();
-        userResponse.setId(savedUser.getId());
-        userResponse.setPassword(savedUser.getPassword());
-        userResponse.setEmail(savedUser.getEmail());
-        userResponse.setFirstName(savedUser.getFirstName());
-        userResponse.setLastName(savedUser.getLastName());
-        userResponse.setCreateAt(savedUser.getCreateAt());
-        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+        return getUserResponse(savedUser);
+    }
 
+    private static UserResponse getUserResponse(User existingUser) {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(existingUser.getId());
+        userResponse.setKeycloakId(existingUser.getKeycloakId());
+        userResponse.setPassword(existingUser.getPassword());
+        userResponse.setEmail(existingUser.getEmail());
+        userResponse.setFirstName(existingUser.getFirstName());
+        userResponse.setLastName(existingUser.getLastName());
+        userResponse.setCreateAt(existingUser.getCreateAt());
+        userResponse.setUpdatedAt(existingUser.getUpdatedAt());
         return userResponse;
     }
 
@@ -46,20 +52,11 @@ public class UserService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        UserResponse userResponse = new UserResponse();
-        userResponse.setId(user.getId());
-        userResponse.setPassword(user.getPassword());
-        userResponse.setEmail(user.getEmail());
-        userResponse.setFirstName(user.getFirstName());
-        userResponse.setLastName(user.getLastName());
-        userResponse.setCreateAt(user.getCreateAt());
-        userResponse.setUpdatedAt(user.getUpdatedAt());
-
-        return userResponse;
+        return getUserResponse(user);
     }
 
     public Boolean exitsByUserId(String userId) {
         log.info("Calling user validation API for userId: {}", userId);
-        return userRepository.existsById(userId);
+        return userRepository.existsByKeycloakId(userId);
     }
 }
